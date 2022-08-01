ea8a14f1910042b2d8125ab337d073a2b31829e3
Eric <ettrudell17@gmail.com>
2022-08-01 06:43:09 -0600

    first commit

---
+++ conditionals.js
@@ -0,0 +1,12 @@
+console.log("Algorithm Challenges");
+
+// var countPositives = 0;
+// var numbers = [3, 4, -2, 7, 16, -8, 0];
+// console.log("there are " + countPositives + " positive values");
+
+var countPositives = 0;
+var numbers = [3, 4, -2, 7, 16, -8, 0];
+    
+for()
+    
+console.log("there are " + countPositives + " positive values");
